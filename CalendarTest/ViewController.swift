3c59a47b5a8fa378c5bd7101e01f6b6ac4559e18
SonHanSol1 <gksthf7869@naver.com>
2022-02-04 15:19:48 +0900

    test Calendar

--- CalendarTest/ViewController.swift
+++ CalendarTest/ViewController.swift
@@ -1,19 +1,92 @@
-//
-//  ViewController.swift
-//  CalendarTest
-//
-//  Created by 손한솔 on 2022/01/25.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource
+{
 
+    @IBOutlet weak var monthLabel: UILabel!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    var selectedDate = Date()
+    var totalSquares = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setCellsView()
+        setMonthView()
+    }
+    
+    func setCellsView() // collectionView 크기 조절
+    {
+        // 크기 맞춤
+        let width = (collectionView.frame.size.width - 2) / 8
+        let height = (collectionView.frame.size.height - 2) / 8
+        
+        let flowLayout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        flowLayout.itemSize = CGSize(width: width, height: height)
+    }
+    
+    func setMonthView() // 달력 날짜 표시 함수
+    {
+        totalSquares.removeAll() // 초기화하고 시작
+        
+        let daysInMonth = CalendarHelper().daysInMonth(date: selectedDate)
+        let firstDayOfMonth = CalendarHelper().firstOfMonth(date: selectedDate)
+        let startingSpaces = CalendarHelper().weekDay(date: firstDayOfMonth)
+        
+        var count: Int = 1  // 달력 날짜 시작 1
+        
+        while(count <= 42) // count 1부터 달력의 끝 숫자까지
+        {
+            if(count <= startingSpaces || count - startingSpaces > daysInMonth)
+            {
+                totalSquares.append("")
+            }
+            else
+            {
+                totalSquares.append(String(count - startingSpaces))
+            }
+            count += 1
+        }
+        
+        // 상단 Label 날짜 출력
+        monthLabel.text = CalendarHelper().yearString(date: selectedDate)
+        //+ " " + CalendarHelper().monthString(date: selectedDate)
+        collectionView.reloadData()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        totalSquares.count
     }
 
+    
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calCell", for: indexPath) as! CalendarCell
+        
+        cell.dayOfMonth.text = totalSquares[indexPath.item]
+        return cell
+    }
 
+    
+    // 왼쪽 월 단위 이동 버튼
+    @IBAction func previousMonth(_ sender: Any)
+    {
+        selectedDate = CalendarHelper().minusMonth(date: selectedDate)
+        setMonthView()
+    }
+    
+    // 오른쪽 월 단위 이동 버튼
+    @IBAction func nextMonth(_ sender: Any)
+    {
+        selectedDate = CalendarHelper().plusMonth(date: selectedDate)
+        setMonthView()
+    }
+    
+    override open var shouldAutorotate: Bool
+    {
+        return false
+    }
 }
 
